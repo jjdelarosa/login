2d6b2329fab7b108ef67636afad9e8564dd5f702
John De La Rosa <john.delarosa@ideapublicschools.org>
2020-02-25 23:22:02 -0600

    Remove firebase keys

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/firebase-auth'
 import * as firebaseui from 'firebaseui'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDKr3WRNDvcpEPRTGO_RGpl3Bh4WppiUMw',
-  authDomain: 'loginpage-a25f4.firebaseapp.com',
-  databaseURL: 'https://loginpage-a25f4.firebaseio.com',
-  projectId: 'loginpage-a25f4',
-  storageBucket: 'loginpage-a25f4.appspot.com',
-  messagingSenderId: '945713729728',
-  appId: '1:945713729728:web:67ecd6994bd8eefcf31d25'
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain, 
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
 }
 
 firebase.initializeApp(firebaseConfig)
